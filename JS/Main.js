f221f590df4ce9d7f98224546b9516e570a8cb4e
Ghorbani-Dev.ir <ghorbani.dev1985@gmail.com>
2023-11-24 12:58:00 +0330

    feat: deverlop change unit function

--- JS/Main.js
+++ JS/Main.js
@@ -1,32 +1,49 @@
 let $ = document;
-let ConvertorInput = $.getElementById("ConvertorInput");
-let ConvertorResult = $.getElementById("ConvertorResult");
-let ConvertBtn = $.getElementById("ConvertBtn");
-let ChangeUnitBtn = $.getElementById("ChangeUnitBtn");
-let ResetBtn = $.getElementById("ResetBtn");
-let ConvertorTitle = $.getElementById("ConvertorTitle");
+let ConvertorInput = $.querySelector("#ConvertorInput");
+let ConvertorResult = $.querySelector("#ConvertorResult");
+let ConvertBtn = $.querySelector("#ConvertBtn");
+let ChangeUnitBtn = $.querySelector("#ChangeUnitBtn");
+let ResetBtn = $.querySelector("#ResetBtn");
+let ConvertorTitle = $.querySelector("#ConvertorTitle");
 
-let ChangeUnit = true;
+let ChangeUnitFlag = true;
 
 function TempConvert() {
     let InputValue = ConvertorInput.value;
     if(InputValue === ""){
         ConvertorResult.innerHTML = `<div class="Result__Wrapper__Error"><span class="convertor__result">لطفا مقدار عددی وارد نمایید</span></div>`;
         ConvertorInput.focus();
-  }else if (ChangeUnit) {
-    ConvertorResult.classList.remove('text-rose');
+  }else if (ChangeUnitFlag) {
     let ConvertFahrenheitToCelsius = (InputValue - 32) * (5 / 9).toFixed(2);
     ConvertorResult.innerHTML = `<div class="Result__Wrapper"><span class="convertor__result">هر ${InputValue} درجه فارنهایت ${ConvertFahrenheitToCelsius} درجه سلسیوس می باشد</span></div>`;
     ConvertorInput.value = "";
 } else {
-    ConvertorResult.classList.remove('text-rose');
     let ConvertCelsiusToFahrenheit = ((InputValue * 1.8) + 32).toFixed(2);
     ConvertorResult.innerHTML = `<div class="Result__Wrapper"><span class="convertor__result">هر ${InputValue} درجه سلسیوس  ${ConvertCelsiusToFahrenheit} درجه فارنهایت می باشد</span></div>`;
     ConvertorInput.value = "";
 }
-ChangeUnit = false;
+ChangeUnitFlag = false;
 };
-
+function ChangeUnit() {
+  ConvertorResult.classList.remove('text-rose');
+if ( $.title === "°F به °C") {
+  ChangeUnitFlag = true;
+  ConvertorTitle.innerHTML = "تبدیل °C به °F و برعکس";
+  $.title = "°C به °F";
+  ConvertorInput.setAttribute("placeholder", "تبدیل °F به  °C...");
+  ConvertorResult.innerHTML = `<div class="Result__Wrapper"><span class="convertor__result">
+   واحد از فارنهایت
+    به سلسیوس تغییر یافت</span></div>`;
+    ConvertorInput.focus();
+} else {
+  ChangeUnitFlag = false;
+  ConvertorTitle.innerHTML = "تبدیل °F به °C و برعکس";
+  $.title = "°F به °C";
+  ConvertorInput.setAttribute("placeholder", "تبدیل °C به °F ...");
+  ConvertorResult.innerHTML = `<div class="Result__Wrapper"><span class="convertor__result"> واحد از سلسیوس به فارنهایت تغییر یافت</span></div>`;
+  ConvertorInput.focus();
+}
+};
 
 
 //EventListener
@@ -36,3 +53,4 @@ ConvertorInput.addEventListener('keydown' , (e) => {
    TempConvert() 
   }
 })
+ChangeUnitBtn.addEventListener('click' , ChangeUnit)
