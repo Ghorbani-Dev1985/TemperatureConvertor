ee15ba5f218c18b476650eddbf831faade570cfd
Ghorbani-Dev.ir <ghorbani.dev1985@gmail.com>
2023-11-24 12:18:46 +0330

    feat: select elements in js file

---
+++ JS/Main.js
@@ -0,0 +1,7 @@
+let $ = document;
+let ConvertorInput = $.getElementById("ConvertorInput");
+let ConvertorResult = $.getElementById("ConvertorResult");
+let ConvertBtn = $.getElementById("ConvertBtn");
+let ChangeUnitBtn = $.getElementById("ChangeUnitBtn");
+let ResetBtn = $.getElementById("ResetBtn");
+let ConvertorTitle = $.getElementById("ConvertorTitle");
